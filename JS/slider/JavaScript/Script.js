eecd412392c0b4a0e2f02ece527e9229208c6fad
congtu96 <congtu96@gmail.com>
2017-07-27 13:08:07 +0700

    update js exercies 1

--- JS/slider/JavaScript/Script.js
+++ JS/slider/JavaScript/Script.js
@@ -1,10 +1,12 @@
 var IndexCurrent = 0; //Default value when load page
 var loop = true;  // Loop is start
 var showbutton =true;  //Show button arrow
-var duration = 3000; //time loop 
+var duration = 5000; //time loop 
+var call = true;//Flag to call next or previous image
 //Array images taken from class name image
 var List_img = document.getElementsByClassName("slider")[0].getElementsByClassName("images");
-var call = true;//Flag to call next or previous image
+//Array point status following slide image
+var Bar_status= document.getElementsByClassName("status")[0].getElementsByClassName("point");
 //func set a image
 function setImage(indexOld){
      for(i = 0; i < List_img.length; i++){
@@ -23,7 +25,33 @@ function setImage(indexOld){
             }
             opacity += 0.02;
             opacityImageOld -= 0.02;    
-    }, duration / 100); 
+    }, duration / 200); 
+}
+//mouse event
+function MouseIn(n) {
+    loop=false;
+    call=false;
+    for (i=0; i<Bar_status.length;i++)
+        if (i==n)
+            Bar_status[i].style.opacity=1;
+        else Bar_status[i].style.opacity=0.3;
+}
+function MouseOut(){
+    loop=true;
+    call=true;
+    for (i=0; i<Bar_status.length;i++)
+        Bar_status[i].style.opacity=0.3;
+}
+//Select picture under slide
+function Select(n){
+    oop=false;
+    call=false;
+    IndexCurrent=n;
+    for(i = 0; i < List_img.length; i++){
+         if(i != n)
+            List_img[i].style.opacity = 0;
+    }
+     List_img[n].style.opacity = 1;
 }
 //func get next image
 function getNextImage(){
@@ -59,7 +87,7 @@ function getPrevImage(){
 function initSlider(){
     List_img[IndexCurrent].style.opacity = 1;
     if(loop){
-        setInterval(getNextImage, duration);
+            setInterval(getNextImage, duration);
     }
 }
 
