92e5ac79ab4c3c004d02f7967fda60162c291041
congtu96 <congtu96@gmail.com>
2017-08-16 17:53:03 +0700

    upload link exercise 1 and review web responsive

---
+++ dp/slider/JavaScript/Script.js
@@ -0,0 +1,85 @@
+var ARR_IMG = $(".images");//Array image on slide
+var ARR_IMG_mini = $(".point");//Array image on status bar
+var LIST_IMG = $(".slider");
+var ARR_IMG_CURRENT = 0;//Variable index img on slider
+var TIME_AUTO = 3000;//3s
+var TIME;// Variable time for slide
+var CHECK_MOUSE = true;//Check mouseover and mouseout
+
+/*Event click*/
+$(".pre").click(function() {
+	ARR_IMG_CURRENT--;
+	console.log(ARR_IMG_CURRENT);
+	if (ARR_IMG_CURRENT < 0) {
+		ARR_IMG_CURRENT = ARR_IMG.length - 1;
+	}
+	showARR_IMG();
+});
+$(".next").click(function() {
+	nextARR_IMG();
+});
+$(".point").click(function () {
+	ARR_IMG_CURRENT = ARR_IMG_mini.index(this);
+	console.log(ARR_IMG_CURRENT);
+	showARR_IMG();
+});
+$(".point").mouseenter(function () {
+	CHECK_MOUSE = false;
+	blurARR_IMG(ARR_IMG_mini.index(this));
+});
+$(".point").mouseleave(function () {
+	CHECK_MOUSE = true;
+	blurARR_IMG(ARR_IMG_CURRENT);
+});
+/**
+* Next image
+*/
+function nextARR_IMG() {
+	
+	ARR_IMG_CURRENT++;
+	if (ARR_IMG_CURRENT > ARR_IMG.length - 1) {
+		ARR_IMG_CURRENT = 0;
+	}
+	
+	showARR_IMG();
+}
+/**
+* Show image
+*/
+function showARR_IMG() {
+	for (var i = 0; i < ARR_IMG.length; i++) {
+		$(".ARR_IMG").eq(i).hide();
+		if (CHECK_MOUSE) {
+			blurARR_IMG(i);
+		}
+	}
+	ARR_IMG.eq(ARR_IMG_CURRENT).show();
+	if (CHECK_MOUSE) {
+		blurARR_IMG(ARR_IMG_CURRENT);
+	}
+	var widthImg = $(".images").width();
+	LIST_IMG[ARR_IMG_CURRENT].animate({left: "-=" + (widthImg)}, 500);
+	Loop();
+}
+/** 
+ * Effect
+ * @param {number}
+ * number: index img to show
+ */
+ function blurARR_IMG(number) {
+ 	//ARR_IMG.animate({left: '200px'});
+ 	ARR_IMG.css("opacity","0");
+ 	ARR_IMG.eq(number).css("opacity","1");
+ 	ARR_IMG_mini.css("opacity","0.5");
+ 	ARR_IMG_mini.eq(number).css("opacity","1");
+ }
+ /**
+ * Loop of slide
+ */
+function Loop() {
+	clearTimeout(TIME);
+	TIME = setTimeout(function() {
+		nextARR_IMG();
+	}, TIME_AUTO);
+}
+showARR_IMG();
